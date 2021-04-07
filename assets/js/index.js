6219359e239b67eb70a1341e0bf4f1245f2995a2
Kate <katebondarenkozp@gmail.com>
2021-04-07 17:13:31 +0300

    work capitalize

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * @param {string} str
+ * @returns string
+ */
+
+function capitalize(str){
+  return str.split(" ")
+            .map((item) => item[0].toUpperCase() + item.slice(1))
+            .join(" ");
+}
+
+const sentences = [
+  "long time no see",
+  "just for the record",
+  "oh, that. That explains it",
+  "things happen"
+];
+
+sentences.forEach((item) => console.log(capitalize(item)));
